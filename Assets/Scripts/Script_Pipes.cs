aecbb6a6e0ee1057c10507f2d69c4ec3dd04f995
Malte Mittrowann <43847487+MalteMittrowann@users.noreply.github.com>
2024-01-23 18:19:06 +0100

    Corners and random length
     - Corners: Instantiate a sphere at the end of the pipe
     - Have the pipes have random lengths
    
    ToDo:
     - Check for the boundaries of the level
     - Prevent collisions

--- Assets/Scripts/Script_Pipes.cs
+++ Assets/Scripts/Script_Pipes.cs
@@ -11,8 +11,8 @@ public class Script_Pipes : MonoBehaviour
     public int minimalTransform = -20;
     public int maximumTransform = 20;
 
-    public int secondsToSpawnNewPipe = 1;
-    public int secondsToRenewTheScene = 30;
+    public float secondsToSpawnNewPipe = 1.0f;
+    public float secondsToRenewTheScene = 30.0f;
 
     // Private variables:
     private Vector3 currentPosition = new Vector3(0,0,0);
@@ -25,7 +25,7 @@ public class Script_Pipes : MonoBehaviour
         Vector3 startSpawnPosition = new Vector3 (Random.Range(-10, 11), Random.Range(-5, 6), Random.Range(0, 21));
         currentPosition = startSpawnPosition;
         GameObject startSphere = Instantiate(sphere, startSpawnPosition, Quaternion.identity, this.transform);
-        InvokeRepeating("CreatePipes", 0, secondsToSpawnNewPipe);
+        InvokeRepeating("CreatePipes", 0.5f, secondsToSpawnNewPipe);
         InvokeRepeating("StartNew", 0, secondsToRenewTheScene);
     }
 
@@ -57,21 +57,36 @@ public class Script_Pipes : MonoBehaviour
     void GoPositiveX ()
     {
         // nextDirection = 0
-        Instantiate(cylinder, new Vector3(currentPosition.x + 1, currentPosition.y, currentPosition.z), Quaternion.Euler(new Vector3(0, 0, 270)), this.transform);
+        int length = Random.Range(1, 21);
+
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x + (length / 2), currentPosition.y, currentPosition.z), Quaternion.Euler(new Vector3(0, 0, 270)), this.transform) as GameObject;
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x + length, currentPosition.y, currentPosition.z), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(length, 0, 0);
         nextDirection = Random.Range(2,6);
     }
 
     void GoNegativeX ()
     {
         // nextDirection = 1
-        Instantiate(cylinder, new Vector3(currentPosition.x - 1, currentPosition.y, currentPosition.z), Quaternion.Euler(new Vector3(0, 0, 90)), this.transform);
+        int length = Random.Range(1, 21);
+
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x - (length / 2), currentPosition.y, currentPosition.z), Quaternion.Euler(new Vector3(0, 0, 90)), this.transform);
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x - length, currentPosition.y, currentPosition.z), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(-length, 0, 0);
         nextDirection = Random.Range(2,6);
     }
 
     void GoPositiveY ()
     {
         // nextDirection = 2
-        Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y + 1, currentPosition.z), Quaternion.identity, this.transform);
+        int length = Random.Range(1, 21);
+        
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y + (length / 2), currentPosition.z), Quaternion.identity, this.transform);
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x, currentPosition.y + length, currentPosition.z), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(0, length, 0);
         int randomNumber = Random.Range(0,2);
         if (randomNumber == 0) {
             nextDirection = Random.Range(0,2);
@@ -83,7 +98,12 @@ public class Script_Pipes : MonoBehaviour
     void GoNegativeY ()
     {
         // nextDirection = 3
-        Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y - 1, currentPosition.z), Quaternion.identity, this.transform);
+        int length = Random.Range(1, 21);
+
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y - (length / 2), currentPosition.z), Quaternion.identity, this.transform);
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x, currentPosition.y - length, currentPosition.z), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(0, -length, 0);
         int randomNumber = Random.Range(0,2);
         if (randomNumber == 0) {
             nextDirection = Random.Range(0,2);
@@ -95,14 +115,24 @@ public class Script_Pipes : MonoBehaviour
     void GoPositiveZ ()
     {
         // nextDirection = 4
-        Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + 1), Quaternion.Euler(new Vector3(270, 0, 0)), this.transform);
+        int length = Random.Range(1, 21);
+
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + (length / 2)), Quaternion.Euler(new Vector3(270, 0, 0)), this.transform);
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + length), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(0, 0, length);
         nextDirection = Random.Range(0,4);
     }
 
     void GoNegativeZ ()
     {
         // nextDirection = 5
-        Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z - 1), Quaternion.Euler(new Vector3(90, 0, 0)), this.transform);
+        int length = Random.Range(1, 21);
+
+        GameObject pipe = Instantiate(cylinder, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z - (length / 2)), Quaternion.Euler(new Vector3(90, 0, 0)), this.transform);
+        pipe.transform.localScale = new Vector3(1, (length / 2), 1);
+        Instantiate(sphere, new Vector3(currentPosition.x, currentPosition.y, currentPosition.z - length), Quaternion.identity, this.transform);
+        currentPosition += new Vector3(0, 0, -length);
         nextDirection = Random.Range(0,4);
     }
 
